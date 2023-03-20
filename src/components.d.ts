9a3046abb7d6ced02fd973aa22c7dab346ba7813
cheanai <lc1308749008@126.com>
2023-03-20 09:05:56 +0800

    11111111

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,7 +9,6 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Head: typeof import('./components/head.vue')['default']
     HeadTitle: typeof import('./components/headTitle.vue')['default']
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     Meau: typeof import('./components/meau.vue')['default']
